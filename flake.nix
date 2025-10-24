d89cdfd6e0b31bb9202bc8fcecbeca4cc6913512
Shantanu Singh <shsingh@broadinstitute.org>
2025-10-24 18:25:30 -0400

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "2025 Best Practice: Pure uv for Python/PyPI Development on NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312
+            uv
+            git
+            # System dependencies for binary wheels
+            zlib
+            stdenv.cc.cc.lib
+            # CBC solver for snakemake
+            cbc
+            # Tools for fixture generation
+            s5cmd
+            parallel
+          ];
+
+          # shellHook = ''
+          #   echo "uv Python Development Environment"
+          #   unset PYTHONPATH
+          #   uv sync
+          # '';
+
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
+            pkgs.zlib
+            pkgs.stdenv.cc.cc.lib
+          ]}";
+        };
+      });
+}
